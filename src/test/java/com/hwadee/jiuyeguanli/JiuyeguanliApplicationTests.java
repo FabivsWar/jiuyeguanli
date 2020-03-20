a9f9c9e32a8a68bda430da3108466b677e9343fb
Li-Zhenghua <43814319+Li-Zhenghua@users.noreply.github.com>
2020-03-20 10:59:17 +0800

    重新搭建项目结构

---
+++ src/test/java/com/hwadee/jiuyeguanli/JiuyeguanliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwadee.jiuyeguanli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiuyeguanliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
